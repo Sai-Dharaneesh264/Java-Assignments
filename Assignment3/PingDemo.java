cde4ef77cabd9e043fa8696406c7b743b02391a2
Sai-Dharaneesh264 <saidharaneesh.gurram@zemosolabs.com>
2022-04-20 12:17:35 +0530

    modified streams Assignments

--- Assignment3/PingDemo.java
+++ Assignment3/PingDemo.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class PingDemo {
-    public static void pingResult(String command)  {
+    public static void pingResult(String command) throws IOException {
         String pingResult = "";
         BufferedReader inputStream = null;
         try {
@@ -21,11 +21,11 @@ public class PingDemo {
         } catch (Exception e) {
             e.printStackTrace();
         }
-//        finally {
-//            inputStream.close();
-//        }
+        finally {
+            inputStream.close();
+        }
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the IP Address : ");
         String ipAddress = scanner.nextLine();
